7066f9e4522df7f98bf23870a6b9dfd6c92231bb
meiwj <meiweijia@gmail.com>
2025-03-31 13:34:34 +0800

    Enable multithreaded compilation

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-ndk-build -C module || exit 1
+ndk-build -j$(nproc) -C module V=1 || exit 1
 
 mkdir -p magisk/zygisk
 
@@ -19,5 +19,5 @@ name=`grep '^id=' module.prop | cut -d= -f2`
 rm -rf ../build
 mkdir -p ../build
 
-zip -r9 ../build/hmspush-zygisk-$version.zip .
+zip -r9 ../build/device-spoof-zygisk-$version.zip .
 popd
